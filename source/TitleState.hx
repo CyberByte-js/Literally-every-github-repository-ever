e083fb09a39dd45ce3640dfb662b047cf0d0ca67
Riskerman21 <61993263+Riskerman21@users.noreply.github.com>
2021-07-30 20:11:11 +1000

    coders rule (pls change me)

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -224,7 +224,7 @@ class TitleState extends MusicBeatState
 		blackScreen = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		credGroup.add(blackScreen);
 
-		credTextShit = new Alphabet(0, 0, "ninjamuffin99\nPhantomArcade\nkawaisprite\nevilsk8er", true);
+		credTextShit = new Alphabet(0, 0, "Riskerman\CyberByte\funkycode-time", true);
 		credTextShit.screenCenter();
 
 		// credTextShit.alignment = CENTER;
